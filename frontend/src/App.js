ca941d7407924a9cd1acfb7e4b9a46327d27b1bf
Sadko-o <r.saduakhas@gmail.com>
2022-10-18 02:10:57 +0600

    home header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import HomeHeader from './components/headers/HomeHeader'
+import "./App.css"
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <HomeHeader/>
+    </>
   )
 }
 
