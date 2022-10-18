48a7f8916803c126be27a94dc56bd65a8275ba58
Sadko-o <r.saduakhas@gmail.com>
2022-10-19 01:11:22 +0600

    db connected and tested

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 require("dotenv").config()
 
-
 const port = process.env.PORT || 8080
+const db   = process.env.DATABASE
+
+mongoose.connect( db ).then(() => console.log("Mongoose is ON")).catch((error) => console.log(error))
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Хеееллоу ночной жор')
