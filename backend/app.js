b1f3fabef5f37ace45cec49d7bf86ca3190c795f
Sadko-o <r.saduakhas@gmail.com>
2022-10-19 17:35:39 +0600

    route UserController

--- backend/app.js
+++ backend/app.js
@@ -8,12 +8,13 @@ const db   = process.env.DATABASE
 
 mongoose.connect( db ).then(() => console.log("Mongoose is ON")).catch((error) => console.log(error))
 
+const userRoute = require("./routes/userRoute")
 
 
-
-app.get('/', (req, res) => {
-  res.send('Хеееллоу ночной жор')
-})
+// app.get('/', (req, res) => {
+//   res.send('Хеееллоу ночной жор')
+// })
+app.use("/userRoute",userRoute)
 
 app.listen(port, () => {
   console.log(`App Listening on port ${port}`)
