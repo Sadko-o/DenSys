0068e985568c6a97a46097c66069b565af0f04f3
Sadko-o <r.saduakhas@gmail.com>
2022-10-23 15:24:51 +0600

    tailwind set up

--- frontend/src/components/headers/HomeHeader.jsx
+++ frontend/src/components/headers/HomeHeader.jsx
@@ -1,58 +1,60 @@
-import React, {useState} from 'react';
-import "./homeHeader.css"
+import React from "react";
 
-const HomeHeader = () => {
-    const [toggle, showMenu] = useState(false)
-    
-    return (
-        <header className='header'>
-            <nav className='nav container'>
-                <a href="index.html" className='nav__logo'>
-                    Den.Sys</a>
-                <div className={toggle? "nav__menu show-menu": "nav__menu"}>
-                    <ul className='nav__list grid'>
-                        
-                        <li className='nav__item'>
-                            <a href='#home' className='nav__link active-link'>
-                                <i className='uil uil-estate nav__icon'></i>Home
-                            </a>
-                        </li>
-                        <li className='nav__item'>
-                            <a href='#about' className='nav__link active-link'>
-                                <i className='uil uil-user nav__icon'></i>About
-                            </a>
-                        </li>
-                        <li className='nav__item'>
-                            <a href='#skills' className='nav__link active-link'>
-                                <i className='uil uil-file-alt nav__icon'></i>Skills?
-                            </a>
-                        </li>
-                        <li className='nav__item'>
-                            <a href='#services' className='nav__link active-link'>
-                                <i className='uil uil-briefcase-alt nav__icon'></i>Services
-                            </a>
-                        </li>
-                        <li className='nav__item'>
-                            <a href='#doctors' className='nav__link active-link'>
-                                <i className='uil uil-scenery nav__icon'></i>Doctors
-                            </a>
-                        </li>
-                        <li className='nav__item'>
-                            <a href='#contact' className='nav__link active-link'>
-                                <i className='uil uil-message nav__icon'></i>Contact
-                            </a>
-                        </li>
-                    </ul>
-
-                    <i class="uil uil-times nav__close" onClick={() => showMenu(!toggle)}></i>
-                </div>
-
-                <div className='nav__toggle' onClick={() => showMenu(!toggle)}>
-                    <i class="uil uil-apps"></i>
-                </div>
-            </nav>
-        </header>
-    )
+export default function HomeHeader({ fixed }) {
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
+  return (
+    <>
+      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-purple-500 mb-3">
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <a
+              className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+              href="#pablo"
+            >
+             Densys
+            </a>
+            <button
+              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              <i className="fas fa-bars"></i>
+            </button>
+          </div>
+          <div
+            className={
+              "lg:flex flex-grow items-center" +
+              (navbarOpen ? " flex" : " hidden")
+            }
+            id="example-navbar-danger"
+          >
+            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  href="#pablo"
+                >
+                  <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Share</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  href="#pablo"
+                >
+                  <i className="fab fa-twitter text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Tweet</span>
+                </a>
+              </li>
+              <li className="nav-item">
+              <button className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button"
+                    >
+                Sign I
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
-
-export default HomeHeader
