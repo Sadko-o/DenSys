da66d8510ae3a6a0b7ef397d2878ae9d0a08facf
Sadko-o <r.saduakhas@gmail.com>
2022-10-27 13:17:01 +0600

    shadow

--- frontend/src/components/pages/home/Services.jsx
+++ frontend/src/components/pages/home/Services.jsx
@@ -10,80 +10,70 @@ const Services = () => {
                 </div>
                 <div class="mt-16 grid gap-6 sm:grid-cols-2 lg:grid-cols-3 md:-mx-8">
                     <div class="relative group">
-                        <div aria-hidden="true" class="absolute inset-0 rounded-xl bg-white shadow-xl transition duration-300 group-hover:scale-105 lg:group-hover:scale-110"></div>
-                        <div class="relative bg-white rounded-md text-center space-y-4 p-6 md:p-8">
+                        <div class="relative  bg-white rounded-md text-center space-y-4 p-6 md:p-8 shadow-md transition duration-200 group-hover:scale-105 lg:group-hover:scale-110">
                             <img src={Cardiology} class="w-16 m-auto bg-white" width="512" height="512" alt="burger illustration"/>
                             <h4 class="text-2xl text-gray-800 bg-white font-medium transition group-hover:text-blue-600">Cardiology</h4>
                             <p class="text-gray-600 bg-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium sint hic quo asperiores commodi corporis, tempora soluta nam animi quod quasi voluptatem earum eveniet nesciunt eligendi harum, odio doloribus veniam!</p>
                         </div>
                     </div>
                     <div class="relative group">
-                        <div aria-hidden="true" class="absolute inset-0 rounded-xl bg-white shadow-xl transition duration-300 group-hover:scale-105 lg:group-hover:scale-110"></div>
-                        <div class="relative bg-white rounded-md text-center space-y-4 p-6 md:p-8">
+                        <div class="relative  bg-white rounded-md text-center space-y-4 p-6 md:p-8 shadow-md transition duration-200 group-hover:scale-105 lg:group-hover:scale-110">
                             <img src={Cardiology} class="w-16 m-auto bg-white" width="512" height="512" alt="package-delivery illustration"/>
                             <h4 class="text-2xl text-gray-800 font-medium transition bg-white group-hover:text-blue-600">Cardiology</h4>
-                            <p class="text-gray-600 bg-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae dolores ipsa velit eveniet, perspiciatis ad ea voluptate cupiditate nobis quaerat.</p>
+                            <p class="text-gray-600 bg-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo eius voluptates alias voluptas magni repudiandae corrupti accusamus blanditiis sit illum, pariatur nobis tempora fuga dicta natus quas doloremque adipisci cumque?</p>
                         </div>
                     </div>
-                    <div class="relative group">
-                        <div aria-hidden="true" class="absolute inset-0 rounded-xl bg-white shadow-xl transition duration-300 group-hover:scale-105 lg:group-hover:scale-110"></div>
-                        <div class="relative bg-white rounded-md text-center space-y-4 p-6 md:p-8">
+                    <div class="relative group ">
+                        <div class="relative  bg-white rounded-md text-center space-y-4 p-6 md:p-8 shadow-md transition duration-200 group-hover:scale-105 lg:group-hover:scale-110">
                             <img src={Cardiology} class="w-16 m-auto bg-white" width="512" height="512" alt="package-delivery illustration"/>
                             <h4 class="text-2xl bg-white text-gray-800 font-medium transition group-hover:text-blue-600">Cardiology</h4>
                             <p class="text-gray-600 bg-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque animi quibusdam ea, sunt est, natus sit amet eaque in laudantium optio voluptate saepe autem excepturi sequi porro cum ipsum dolorem?.</p>
                         </div>
                     </div>
                     <div class="relative group lg:hidden">
-                        <div aria-hidden="true" class="absolute inset-0 rounded-xl bg-white shadow-xl transition duration-300 group-hover:scale-105 lg:group-hover:scale-110"></div>
-                        <div class="relative text-center space-y-4 p-6 md:p-8">
+                        <div class="relative  bg-white rounded-md text-center space-y-4 p-6 md:p-8 shadow-md transition duration-200 group-hover:scale-105 lg:group-hover:scale-110">
                             <img src="images/avatars/metal.png" class="w-20 -mt-2 mx-auto" width="512" height="512" alt="package-delivery illustration"/>
                             <h4 class="text-2xl bg-white text-gray-800 font-medium transition group-hover:text-blue-600">Cardiology</h4>
                             <p class="text-gray-600 bg-white">Neque Dolor, alias quae, quibusdam accusantium, laudantium recusandae tenetur fugiat non cum doloribus aperiam voluptates nostrum.</p>
                         </div>
                     </div>
                     <div class="relative group">
-                        <div aria-hidden="true" class="absolute inset-0 rounded-xl bg-white shadow-xl transition duration-300 group-hover:scale-105 lg:group-hover:scale-110"></div>
-                        <div class="relative bg-white rounded-md text-center space-y-4 p-6 md:p-8">
+                        <div class="relative  bg-white rounded-md text-center space-y-4 p-6 md:p-8 shadow-md transition duration-200 group-hover:scale-105 lg:group-hover:scale-110">
                             <img src={Cardiology} class="w-16 m-auto bg-white" width="512" height="512" alt="burger illustration"/>
                             <h4 class="text-2xl text-gray-800 bg-white font-medium transition group-hover:text-blue-600">Cardiology</h4>
                             <p class="text-gray-600 bg-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium sint hic quo asperiores commodi corporis, tempora soluta nam animi quod quasi voluptatem earum eveniet nesciunt eligendi harum, odio doloribus veniam!</p>
                         </div>
                     </div>
                     <div class="relative group">
-                        <div aria-hidden="true" class="absolute inset-0 rounded-xl bg-white shadow-xl transition duration-300 group-hover:scale-105 lg:group-hover:scale-110"></div>
-                        <div class="relative bg-white rounded-md text-center space-y-4 p-6 md:p-8">
+                        <div class="relative  bg-white rounded-md text-center space-y-4 p-6 md:p-8 shadow-md transition duration-200 group-hover:scale-105 lg:group-hover:scale-110">
                             <img src={Cardiology} class="w-16 m-auto bg-white" width="512" height="512" alt="burger illustration"/>
                             <h4 class="text-2xl text-gray-800 bg-white font-medium transition group-hover:text-blue-600">Cardiology</h4>
                             <p class="text-gray-600 bg-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium sint hic quo asperiores commodi corporis, tempora soluta nam animi quod quasi voluptatem earum eveniet nesciunt eligendi harum, odio doloribus veniam!</p>
                         </div>
                     </div>
                     <div class="relative group">
-                        <div aria-hidden="true" class="absolute inset-0 rounded-xl bg-white shadow-xl transition duration-300 group-hover:scale-105 lg:group-hover:scale-110"></div>
-                        <div class="relative bg-white rounded-md text-center space-y-4 p-6 md:p-8">
+                        <div class="relative  bg-white rounded-md text-center space-y-4 p-6 md:p-8 shadow-md transition duration-200 group-hover:scale-105 lg:group-hover:scale-110">
                             <img src={Cardiology} class="w-16 m-auto bg-white" width="512" height="512" alt="burger illustration"/>
                             <h4 class="text-2xl text-gray-800 bg-white font-medium transition group-hover:text-blue-600">Cardiology</h4>
                             <p class="text-gray-600 bg-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium sint hic quo asperiores commodi corporis, tempora soluta nam animi quod quasi voluptatem earum eveniet nesciunt eligendi harum, odio doloribus veniam!</p>
                         </div>
                     </div>
                     <div class="relative group">
-                        <div aria-hidden="true" class="absolute inset-0 rounded-xl bg-white shadow-xl transition duration-300 group-hover:scale-105 lg:group-hover:scale-110"></div>
-                        <div class="relative bg-white rounded-md text-center space-y-4 p-6 md:p-8">
+                        <div class="relative  bg-white rounded-md text-center space-y-4 p-6 md:p-8 shadow-md transition duration-200 group-hover:scale-105 lg:group-hover:scale-110">
                             <img src={Cardiology} class="w-16 m-auto bg-white" width="512" height="512" alt="burger illustration"/>
                             <h4 class="text-2xl text-gray-800 bg-white font-medium transition group-hover:text-blue-600">Cardiology</h4>
                             <p class="text-gray-600 bg-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium sint hic quo asperiores commodi corporis, tempora soluta nam animi quod quasi voluptatem earum eveniet nesciunt eligendi harum, odio doloribus veniam!</p>
                         </div>
                     </div>
                     <div class="relative group">
-                        <div aria-hidden="true" class="absolute inset-0 rounded-xl bg-white shadow-xl transition duration-300 group-hover:scale-105 lg:group-hover:scale-110"></div>
-                        <div class="relative bg-white rounded-md text-center space-y-4 p-6 md:p-8">
+                        <div class="relative  bg-white rounded-md text-center space-y-4 p-6 md:p-8 shadow-md transition duration-200 group-hover:scale-105 lg:group-hover:scale-110">
                             <img src={Cardiology} class="w-16 m-auto bg-white" width="512" height="512" alt="burger illustration"/>
                             <h4 class="text-2xl text-gray-800 bg-white font-medium transition group-hover:text-blue-600">Cardiology</h4>
                             <p class="text-gray-600 bg-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium sint hic quo asperiores commodi corporis, tempora soluta nam animi quod quasi voluptatem earum eveniet nesciunt eligendi harum, odio doloribus veniam!</p>
                         </div>
                     </div>
                     <div class="relative group">
-                        <div aria-hidden="true" class="absolute inset-0 rounded-xl bg-white shadow-xl transition duration-300 group-hover:scale-105 lg:group-hover:scale-110"></div>
-                        <div class="relative bg-white rounded-md text-center space-y-4 p-6 md:p-8">
+                        <div class="relative  bg-white rounded-md text-center space-y-4 p-6 md:p-8 shadow-md transition duration-200 group-hover:scale-105 lg:group-hover:scale-110">
                             <img src={Cardiology} class="w-16 m-auto bg-white" width="512" height="512" alt="burger illustration"/>
                             <h4 class="text-2xl text-gray-800 bg-white font-medium transition group-hover:text-blue-600">Cardiology</h4>
                             <p class="text-gray-600 bg-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium sint hic quo asperiores commodi corporis, tempora soluta nam animi quod quasi voluptatem earum eveniet nesciunt eligendi harum, odio doloribus veniam!</p>
