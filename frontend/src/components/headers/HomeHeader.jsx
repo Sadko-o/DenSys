fa03bf3ad5a2987cd073cfbb3fdcc4cf0bd68802
shellypek <sholpan.bolatzhanova@gmail.com>
2022-10-31 18:18:39 +0600

    update

--- frontend/src/components/headers/HomeHeader.jsx
+++ frontend/src/components/headers/HomeHeader.jsx
@@ -1,39 +1,43 @@
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import { useNavigate } from 'react-router-dom';
+import { Fragment } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { useNavigate } from "react-router-dom";
 
-import logo from "../../assets/logo.svg"
-import {Link} from 'react-router-dom';
+import logo from "../../assets/logo.svg";
+import { Link } from "react-router-dom";
 
 const navigation = [
-  { name: 'About Us', href: '#', current: true },
-  { name: 'Specialists', href: '#', current: false },
-  { name: 'Services', href: '#services', current: false },
-  { name: 'Contact Us', href: '#', current: false },
-]
+  { name: "About Us", href: "#", current: true },
+  { name: "Specialists", href: "#", current: false },
+  { name: "Services", href: "#services", current: false },
+  { name: "Contact Us", href: "#", current: false },
+];
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
-
 export default function HomeHeader() {
-
+  const auth = false;
   const navigate = useNavigate();
 
   const navigateToSignInPage = () => {
-    navigate('/signin')
-  }
+    navigate("/signin");
+  };
   const navigateToHome = () => {
-    navigate('/')
-  }
+    navigate("/");
+  };
 
   return (
-    <Disclosure as="nav" className="main" class="fixed -z-n1 flex justify-between " style={{"width":"100vw"}}>
+    <Disclosure
+      as="nav"
+      className="main"
+      class="fixed -z-n1 flex justify-between "
+      style={{ width: "100vw" }}
+    >
       {({ open }) => (
         <>
-          <div className="w-full sm:px-6 lg:px-8 "  >
+          <div className="w-full sm:px-6 lg:px-8 ">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
@@ -47,15 +51,12 @@ export default function HomeHeader() {
                 </Disclosure.Button>
               </div>
               <div className="flex flex-1 items-center justify-center  sm:items-stretch sm:justify-between">
-                <div className="flex flex-shrink-0 items-center" onClick={navigateToHome}>
-                  <img
-                    className="block h-8 w-auto lg:hidden"
-                    src={logo}
-                  />
-                  <img
-                    className="hidden h-8 w-auto lg:block"
-                    src={logo}
-                  />
+                <div
+                  className="flex flex-shrink-0 items-center"
+                  onClick={navigateToHome}
+                >
+                  <img className="block h-8 w-auto lg:hidden" src={logo} />
+                  <img className="hidden h-8 w-auto lg:block" src={logo} />
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
@@ -63,10 +64,11 @@ export default function HomeHeader() {
                       <a
                         key={item.name}
                         href={item.href}
-                        className={classNames( 'text-[#1B1212] hover:bg-[#2273FF] hover:text-white',
-                          'px-3 py-2 rounded-md text-sm font-medium'
+                        className={classNames(
+                          "text-[#1B1212] hover:bg-[#2273FF] hover:text-white",
+                          "px-3 py-2 rounded-md text-sm font-medium"
                         )}
-                        aria-current={item.current ? 'page' : undefined}
+                        aria-current={item.current ? "page" : undefined}
                       >
                         {item.name}
                       </a>
@@ -78,18 +80,15 @@ export default function HomeHeader() {
                 <div className="inline-flex rounded-md shadow">
                   <a
                     href="#"
-                    className=' shadow-md  text-[black] hover:bg-[blue] hover:text-white px-3  py-2 rounded-md text-sm font-medium'
-                    onClick={navigateToSignInPage}>
+                    className=" shadow-md  text-[black] hover:bg-[blue] hover:text-white px-3  py-2 rounded-md text-sm font-medium"
+                    onClick={navigateToSignInPage}
+                  >
                     Sign In
                   </a>
                 </div>
               </div>
             </div>
-
-
           </div>
-
-
 
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 px-2 pt-2 pb-3">
@@ -99,10 +98,12 @@ export default function HomeHeader() {
                   as="a"
                   href={item.href}
                   className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block px-3 py-2 rounded-md text-base font-medium'
+                    item.current
+                      ? "bg-gray-900 text-white"
+                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                    "block px-3 py-2 rounded-md text-base font-medium"
                   )}
-                  aria-current={item.current ? 'page' : undefined}
+                  aria-current={item.current ? "page" : undefined}
                 >
                   {item.name}
                 </Disclosure.Button>
@@ -112,5 +113,5 @@ export default function HomeHeader() {
         </>
       )}
     </Disclosure>
-  )
+  );
 }
--- frontend/src/components/pages/admin/AdminPage.jsx
+++ frontend/src/components/pages/admin/AdminPage.jsx
@@ -1,16 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import HomeHeader from "../../headers/HomeHeader";
 
 class AdminPage extends Component {
-    render() {
-        return (
-            <div class=" sm:py-8 lg:py-12">
-                <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
-                    <h2 class="text-gray-800 text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-8">Users List</h2>
-                    
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <>
+        <HomeHeader></HomeHeader>
+        <div class=" sm:py-10 lg:py-16">
+          <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
+            <h2 class="text-gray-800 text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-8">
+              Users List
+            </h2>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default AdminPage;
