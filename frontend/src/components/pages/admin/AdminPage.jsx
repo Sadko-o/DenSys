6789628533d0c1b8226093ddc808ca8d650c5894
shellypek <sholpan.bolatzhanova@gmail.com>
2022-10-31 19:03:40 +0600

    updated header

--- frontend/src/components/pages/admin/AdminPage.jsx
+++ frontend/src/components/pages/admin/AdminPage.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import HomeHeader from "../../headers/HomeHeader";
+import AdminHeader from "../../headers/AdminHeader";
 
 class AdminPage extends Component {
   render() {
     return (
       <>
-        <HomeHeader></HomeHeader>
+        <AdminHeader></AdminHeader>
         <div class=" sm:py-10 lg:py-16">
           <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
             <h2 class="text-gray-800 text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-8">
