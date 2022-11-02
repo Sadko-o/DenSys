8ececed9ae829fc04765eb9b7c307c836cfad70b
shellypek <sholpan.bolatzhanova@gmail.com>
2022-11-02 10:17:15 +0600

    just in case to save changes

--- frontend/src/components/pages/admin/RegisterUserPage.jsx
+++ frontend/src/components/pages/admin/RegisterUserPage.jsx
@@ -1,10 +1,138 @@
 import React, { Component } from "react";
 import AdminHeader from "../../headers/AdminHeader";
+import { useEffect, useState } from "react";
+import Admin from "./users/Admin";
+import Doctor from "./users/Doctor";
+import Patient from "./users/Patient";
+import { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 export default function RegisterUserPage() {
+  const [user, setUser] = useState("Select User");
+
+  const [patientContentVisible, setPatientContentVisible] = useState(false);
+  const [adminContentVisible, setAdminContentVisible] = useState(false);
+  const [doctorContentVisible, setDoctorContentVisible] = useState(false);
+
+  useEffect(() => {
+    user === "patient"
+      ? setPatientContentVisible(true)
+      : setPatientContentVisible(false);
+    user === "admin"
+      ? setAdminContentVisible(true)
+      : setAdminContentVisible(false);
+    user === "doctor"
+      ? setDoctorContentVisible(true)
+      : setDoctorContentVisible(false);
+  }, [user]);
+
+  const handleOnChange = (e) => {
+    setUser(e.target.value);
+  };
+  const makeFirstLetterCapital = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
+
   return (
     <>
-      <AdminHeader></AdminHeader>
+      <AdminHeader />
+      <div className="sm:py-10 lg:py-16 flex flex-col items-center justify-center">
+        <div className="mt-4">
+          <select className="" value={user} onChange={handleOnChange}>
+            <option value="selectUser">Select user</option>
+            <option value="admin">Admin</option>
+            <option value="patient">Patient</option>
+            <option value="doctor">Doctor</option>
+          </select>
+        </div>
+        {adminContentVisible && <Admin />}
+        {patientContentVisible && <Patient />}
+        {doctorContentVisible && <Doctor />}
+      </div>
+      <Menu as="div" className="relative inline-block text-left">
+        <div>
+          <Menu.Button className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
+            Options
+            <ChevronDownIcon
+              className="-mr-1 ml-2 h-5 w-5"
+              aria-hidden="true"
+            />
+          </Menu.Button>
+        </div>
+
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="py-1">
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames(
+                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                      "block px-4 py-2 text-sm"
+                    )}
+                  >
+                    Account settings
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames(
+                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                      "block px-4 py-2 text-sm"
+                    )}
+                  >
+                    Support
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames(
+                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                      "block px-4 py-2 text-sm"
+                    )}
+                  >
+                    License
+                  </a>
+                )}
+              </Menu.Item>
+              <form method="POST" action="#">
+                <Menu.Item>
+                  {({ active }) => (
+                    <button
+                      type="submit"
+                      className={classNames(
+                        active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                        "block w-full px-4 py-2 text-left text-sm"
+                      )}
+                    >
+                      Sign out
+                    </button>
+                  )}
+                </Menu.Item>
+              </form>
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
     </>
   );
 }
