40ece7d3e6dbf84d0d67087a0849442afe1731b9
Minilay <minilie88@gmail.com>
2022-11-26 01:57:02 +0600

    Sign In page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,17 @@ import "./App.css";
 import SignIn from "./components/pages/common/SignIn";
 import Home from "./components/pages/home/Home";
 import AdminPage from "./components/pages/admin/AdminPage";
+import DoctorPage from "./components/pages/doctor/DoctorPage";
+import PatientPage from "./components/pages/patient/PatientPage";
 import RegisterUserPage from "./components/pages/admin/RegisterUserPage";
 const App = () => {
   return (
     <Routes>
-      <Route exact path="/" element={<Home/>} />
+      <Route exact path="/" element={<Home />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path="/adminpage" element={<AdminPage />} />
+      <Route path="/doctorpage" element={<DoctorPage />} />
+      <Route path="/patientpage" element={<PatientPage />} />
       <Route path="/registeruser" element={<RegisterUserPage />} />
     </Routes>
   );
